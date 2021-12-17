c4327a92266296480ee2786c677ff4aece04bde0
andydangit <andy.dang.it@gmail.com>
2021-12-17 11:35:41 -0600

    Refactor: Tuple  set how many in an array

--- app.js
+++ app.js
@@ -6,12 +6,16 @@
 var person = {
     name: 'Andy',
     age: 20,
-    hobbies: ['Sports', "Cooking"]
+    hobbies: ['Sports', "Cooking"],
+    role: [2, 'author']
 };
+// person.role.push('admin'); // this is an exception 
+// person.role[1] = 10; // this does not work
 var favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map());  // !!! ERROR !!!
 }
--- app.ts
+++ app.ts
@@ -4,11 +4,21 @@
 //     age: number
 // } 
 
-const person = { 
+const person: { 
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = { 
     name: 'Andy',
     age: 20,
-    hobbies: ['Sports', "Cooking"]
+    hobbies: ['Sports', "Cooking"],
+    role: [2, 'author']  
 };
+
+// person.role.push('admin'); // this is an exception 
+// person.role[1] = 10; // this does not work
+
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
