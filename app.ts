bd3f21514d36b82e5c2e3c485d87c21575fcc54b
andydangit <andy.dang.it@gmail.com>
2021-12-17 15:15:51 -0600

    Refactor: literal  type

--- app.ts
+++ app.ts
@@ -1,15 +1,27 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
-  } else { 
-      result = input1.toString() + input2.toString();
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
   }
   return result;
+//   if (resultConversion === "as-number") {
+//     return +result;
+//   } else {
+//     return result;
+//   }
 }
 
-const combineAge = combine(30, 26);
+const combineAge = combine(30, 26, "as-number");
 console.log(combineAge);
 
-const combineNames = combine("Max", "Anna");
+const combineStringAges = combine("30", "26", "as-number");
+console.log(combineStringAges);
+
+const combineNames = combine("Max", "Anna", "as-text");
 console.log(combineNames);
