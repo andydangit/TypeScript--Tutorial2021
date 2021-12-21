914ec2a66de5cd844077f4b0dd49232c59bda2aa
andydangit <andy.dang.it@gmail.com>
2021-12-21 14:28:48 -0600

    Never Type

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var userInput;
+var userName;
 userInput = 5;
-userInput = 'Max';
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+// This function return Never
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occurred!', 500);
--- app.ts
+++ app.ts
@@ -6,3 +6,13 @@ userInput = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+
+// This function return Never
+function generateError (message: string, code: number): never {
+    throw { message: message, errorCode: code};
+    // while (true) {}
+}
+
+generateError('An error occurred!', 500);
+
